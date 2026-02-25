3ccc8269aff9904aa12c4f82578fe72a755f8e09
Wallace McGee <wm@wolispace.com>
2026-02-25 19:00:17 +1030

    implement a _env.bat

--- server.bat
+++ server.bat
@@ -1,5 +1,8 @@
+@echo off
+:: read in my local settings that sets vars like: set PHP_PATH="W:\My Drive\Apps\php7\php.exe"
+call _env.bat
+
 start "" http://localhost:88/dev.html
 
-start "" "W:\My Drive\Apps\php7\php.exe" -S localhost:88
-::pause
-
+start "" %PHP_PATH% -S localhost:88
+pause
